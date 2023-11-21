f95e80d4cbb15eea499c79dd637e6648cb481b0e
Cedric Bleck <cedricbglx@gmail.com>
2023-11-22 00:25:35 +0100

    added more mustache: profile.mustache, error.mustache, added mappings in LandingController

--- src/main/java/com/project/einHundertAntworten/LandingController.java
+++ src/main/java/com/project/einHundertAntworten/LandingController.java
@@ -1,14 +1,13 @@
 package com.project.einHundertAntworten;
 
-import com.project.einHundertAntworten.User.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LandingController {
 
-    @GetMapping("/")
-    public String landing(Model model) {
+    @GetMapping("/home")
+    public String getLanding() {
         return "index";
     }
 
@@ -16,4 +15,25 @@ public class LandingController {
     public String getQuizpanel() {
         return "quizPanel";
     }
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String getRegister() {
+        return "register";
+    }
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return "profile";
+    }
+
+    @GetMapping("/error")
+    public String getError() {
+        return "errorPage";
+    }
+
 }
