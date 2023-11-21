9b151d8b0b1d5f70331e3bee922dacfdd079a7bb
DenKedi <cedricbglx@gmail.com>
2023-11-21 18:21:11 +0100

    small changes

--- src/main/java/com/project/einHundertAntworten/Misc/SecurityConfig.java
+++ src/main/java/com/project/einHundertAntworten/Misc/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,6 +21,7 @@ public class SecurityConfig  {
         return http.build();
     }
     * */
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
