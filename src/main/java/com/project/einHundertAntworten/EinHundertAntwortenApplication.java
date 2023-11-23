d4ff75c1a908ae25f0f83256f586bcc40718feea
Cedric Bleck <cedricbglx@gmail.com>
2023-11-23 08:39:32 +0100

    2.0

--- src/main/java/com/project/einHundertAntworten/EinHundertAntwortenApplication.java
+++ src/main/java/com/project/einHundertAntworten/EinHundertAntwortenApplication.java
@@ -1,18 +1,8 @@
 package com.project.einHundertAntworten;
-
-import com.mongodb.*;
-import com.mongodb.client.*;
-import com.mongodb.client.model.Filters;
 import com.project.einHundertAntworten.Misc.RsaKeyProperties;
-import org.bson.Document;
-import org.bson.types.ObjectId;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication // (exclude = {SecurityAutoConfiguration.class })
