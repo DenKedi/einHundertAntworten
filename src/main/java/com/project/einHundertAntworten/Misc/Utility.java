d20682136bd3ba79dbc2d93f43a88bbcaef21010
Cedric Bleck <cedricbglx@gmail.com>
2023-11-23 16:21:32 +0100

    register frontend läuft

--- src/main/java/com/project/einHundertAntworten/Misc/Utility.java
+++ src/main/java/com/project/einHundertAntworten/Misc/Utility.java
@@ -5,22 +5,36 @@ import java.util.regex.Pattern;
 
 public class Utility {
 
-    static final String specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\\\]^_`~";
+    static final String specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\\\]^_`~ ";
+    static final String specialCharactersUN = "!\"#$%&'()*+,./:;<=>?@[\\\\]^`~ ";
     static String regex = ".*[" + Pattern.quote(specialCharacters) + "].*";
     static Pattern pattern = Pattern.compile(regex);
     public static final String statusOK = "OK";
     public static String pwMeetsRequirements(String pw){
         if (pw.length()<=6){
-            return("6 or more characters required.");
+            return("Password requires 6 or more characters.");
         }
         Matcher matcher = pattern.matcher(pw);
         if (!matcher.matches()){
-            return("Special characters required");
+            return("Special characters required in Password");
         }
         if (!pw.matches(".*\\d.*")){
-            return("Numbers required");
+            return("Numbers required in Password");
         }
-        return("OK");
+        return(statusOK);
+    }
+    public static String usernameMeetsRequirements(String username){
+        if (username.length()<=3){
+            return("Username requires 3 or more characters.");
+        }
+        Matcher matcher = pattern.matcher(username);
+        if (matcher.matches()){
+            return("Special characters not allowed in Username (except for '_' and '-')");
+        }
+        if (username.equals("Username already exists") || username.equals("Email already exists")){
+            return("Forbidden Username");
+        }
+        return(statusOK);
     }
 
     public static void main(String[] args) {
